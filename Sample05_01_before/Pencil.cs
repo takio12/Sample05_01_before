e34023b68c7ecc394f1c0fcc5c0d5853ed2d6eaa
TAKIO-PC\takio <taka21centry@gmail.com>
2017-10-04 00:46:41 +0900

    プロジェクト ファイルを追加します。

---
+++ Sample05_01_before/Pencil.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample05_01_before
+{
+    class Pencil
+    {
+        private string thickness;
+        private double lead;
+
+        public Pencil(string color,string thickness, double lead)
+        {
+            Color = color;
+            this.thickness = thickness;
+            this.lead = lead;
+        }
+        public Pencil()
+        {
+            Color = "青";
+            this.thickness = "普通";
+            this.lead = 30.0;
+        }
+
+        public string Color { get; set; }
+        public string Thickness
+        {
+            get { return thickness; }
+            set {
+                if (value == "細い" || value == "普通" || value == "太い")
+                {
+                    thickness = value;
+                } else
+                {
+                    thickness = "普通";
+                }
+            }
+        }
+        public double Lead
+        {
+            get { return lead; }
+            set
+            {
+                if (value >= 0.0)
+                {
+                    lead = value;
+                }
+                else
+                {
+                    lead = 100;
+                }
+            }
+        }
+        // メソッド
+        // 線を引く
+        // 戻り値：線の長さ
+        public int Draw(out double lead)
+        {
+            Console.WriteLine("{0} {1}", Color, Thickness);
+            Line(10);
+            Lead -= 0.1;
+            lead = Lead;
+            return 10;
+        }
+        // 仮引数　length : 長さ　lead：芯の残量
+        // 戻り値：線の長さ
+        public int Draw(int length, out double lead)
+        {
+            Console.WriteLine("{0} {1}", Color, Thickness);
+            Line(length);
+            Lead -= 0.01 * length;
+            lead = Lead;
+            return length;
+        }
+        // 線を描く　length : 長さ
+        private void Line(int length)
+        {
+            for (int i = 1; i <= length; i++)
+            {
+                Console.Write("－");
+            }
+            Console.WriteLine();
+        }
+
+        // 色を塗る
+        // 仮引数　lead：芯の残量
+        // 戻り値：塗りつぶした面積
+        public int Fill(out double lead)
+        {
+            Console.WriteLine("{0} {1}", Color, Thickness);
+            Rectangle(5, 1);
+
+            Lead -= 0.1 * 5;
+            lead = Lead;
+            return 5;
+        }
+
+        // 仮引数　width : 幅height : 高さ　lead：芯の残量
+        // 戻り値：塗りつぶした面積
+        public int Fill(int width, int height, out double lead)
+        {
+            Console.WriteLine("{0} {1}", Color, Thickness);
+            Rectangle(width, height);
+
+            Lead -= 0.1 * width * height;
+            lead = Lead;
+            return width * height;
+        }
+        // 長方形を描く　width : 幅　height : 高さ
+        private void Rectangle(int width, int height)
+        {
+            for (int i = 1; i <= height; i++)
+            {
+                for (int j = 1; j <= width; j++)
+                {
+                    Console.Write("■");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Sample05_01_before/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample05_01_before
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // インスタンスの生成
+            Pencil p1 = new Pencil("黄","細い",100.0);
+            Pencil p2 = new Pencil("緑","太い",50.0);
+            Pencil p3 = new Pencil();
+
+
+            // 線を引いたり色を塗ったりする
+            double lead;
+            int len = p1.Draw(out lead);
+            Console.WriteLine("長さ ＝ {0} 残りの芯の量 = {1}\n", len, lead);
+            int area = p2.Fill(out lead);
+            Console.WriteLine("面積 ＝ {0} 残りの芯の量 = {1}\n", area, lead);
+            len = p3.Draw(out lead);
+            Console.WriteLine("長さ ＝ {0} 残りの芯の量 = {1}\n", len, lead);
+
+            area = p1.Fill(2, 3, out lead);
+            Console.WriteLine("面積 ＝ {0} 残りの芯の量 = {1}\n", area, lead);
+            len = p2.Draw(30, out lead);
+            Console.WriteLine("長さ ＝ {0} 残りの芯の量 = {1}\n", len, lead);
+            int w = 5, h = 8;
+            area = p3.Fill(w, h, out lead);
+            Console.WriteLine("面積 ＝ {0} 残りの芯の量 = {1}\n", area, lead);
+        }
+    }
+}
+
